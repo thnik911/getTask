b998878b400ea675ff9308ef82cd635a8221d8b9
Никита Лукьяница <59867180+thnik911@users.noreply.github.com>
2022-04-12 19:21:44 +0500

    Update getTask.php
    Убрал запись в лог по ходу выполнения скрипта

--- getTask.php
+++ getTask.php
@@ -1,112 +1,111 @@
 <?php
-ini_set("display_errors","1");
-ini_set("display_startup_errors","1");
+ini_set("display_errors", "1");
+ini_set("display_startup_errors", "1");
 ini_set('error_reporting', E_ALL);
 
-//writetolog($_REQUEST, 'new request');
-
-//AUTH 
-require_once('auth.php');
+//AUTH
+require_once 'auth.php';
 
 $task = $_REQUEST['data']['FIELDS_AFTER']['ID'];
 $event = $_REQUEST['event'];
 
-
-if($event == 'ONTASKADD'){
+if ($event == 'ONTASKADD') {
     sleep(3);
     $taskGet = executeREST(
         'task.item.getdata',
         array(
-                $task,		
-    
-            ),
-    $domain, $auth, $user);
-    
+            $task,
+
+        ),
+        $domain, $auth, $user);
+
     $tags = $taskGet['result']['TAGS'][0];
     $deal = $taskGet['result']['UF_CRM_TASK'][0];
 
-    if(!empty($deal) and $tags != 'Автоматика'){
+    if (!empty($deal) and $tags != 'Автоматика') {
         preg_match_all("/\d+/", $deal, $matches);
         $deal = $matches[0][0];
         $getdeal = executeREST(
             'crm.deal.get',
             array(
-                    'ID' => $deal,	
-                    ),
-        $domain, $auth, $user);
+                'ID' => $deal,
+            ),
+            $domain, $auth, $user);
 
         $idOfBP = $getdeal['result']['UF_CRM_1647844191'];
-        if(empty($idOfBP)){
+        if (empty($idOfBP)) {
             exit;
         }
 
         $listGet = executeREST(
             'bizproc.workflow.instances',
             array(
-                'select' => array('ID',),		
-                'order' => array('STARTED' => 'DESC',),
-                'filter' => array('DOCUMENT_ID' => $idOfBP,),
-                ),
-        $domain, $auth, $user);
-        
+                'select' => array('ID'),
+                'order' => array('STARTED' => 'DESC'),
+                'filter' => array('DOCUMENT_ID' => $idOfBP),
+            ),
+            $domain, $auth, $user);
+
         $idOfWf = $listGet['result'][0]['ID'];
 
         $killBP = executeREST(
             'bizproc.workflow.terminate',
             array(
-                    'ID' => $idOfWf,	
-                    ),
-        $domain, $auth, $user);
+                'ID' => $idOfWf,
+            ),
+            $domain, $auth, $user);
 
-    }else{
+
+    } else {
 
     }
-}else{
+} else {
     $taskGet = executeREST(
         'task.item.getdata',
         array(
-                $task,		
-    
-            ),
-    $domain, $auth, $user);
-    
+            $task,
+
+        ),
+        $domain, $auth, $user);
+
     $tags = $taskGet['result']['TAGS'][0];
     $deal = $taskGet['result']['UF_CRM_TASK'][0];
     $taskStatus = $taskGet['result']['STATUS'];
     preg_match_all("/\d+/", $deal, $matches);
     $deal = $matches[0][0];
-    
-    if(!empty($deal) and $tags != 'Автоматика' and $taskStatus == 5){
-            
+
+    if (!empty($deal) and $tags != 'Автоматика' and $taskStatus == 5) {
+
         $getdeal = executeREST(
             'crm.deal.get',
             array(
-                    'ID' => $deal,	
-                    ),
-        $domain, $auth, $user);
+                'ID' => $deal,
+            ),
+            $domain, $auth, $user);
         $idOfBP = $getdeal['result']['UF_CRM_1647844191'];
 
-        if(empty($idOfBP)){
+        if (empty($idOfBP)) {
             exit;
         }
-       
+
         $startworkflow = executeREST(
             'bizproc.workflow.start',
             array(
-                    'TEMPLATE_ID' => '187',	
-                    'DOCUMENT_ID' => array (
-                        'lists', 'BizprocDocument', $idOfBP,
-                    ),
+                'TEMPLATE_ID' => '187',
+                'DOCUMENT_ID' => array(
+                    'lists', 'BizprocDocument', $idOfBP,
                 ),
-        $domain, $auth, $user);
-    
-    }else{
-  
+            ),
+            $domain, $auth, $user);
+
+    } else {
+
     }
 }
 
-function executeREST ($method, array $params, $domain, $auth, $user) {
-    $queryUrl = 'https://'.$domain.'/rest/'.$user.'/'.$auth.'/'.$method.'.json';
+function executeREST($method, array $params, $domain, $auth, $user)
+{
+    $queryUrl = 'https://' . $domain . '/rest/' . $user . '/' . $auth . '/' . $method . '.json';
     $queryData = http_build_query($params);
     $curl = curl_init();
     curl_setopt_array($curl, array(
@@ -121,14 +120,13 @@ function executeREST ($method, array $params, $domain, $auth, $user) {
     curl_close($curl);
 }
 
-function writeToLog($data, $title = '') {
-$log = "\n------------------------\n";
-$log .= date("Y.m.d G:i:s") . "\n";
-$log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
-$log .= print_r($data, 1);
-$log .= "\n------------------------\n";
-file_put_contents(getcwd() . '/gettask.log', $log, FILE_APPEND);
-return true;
+function writeToLog($data, $title = '')
+{
+    $log = "\n------------------------\n";
+    $log .= date("Y.m.d G:i:s") . "\n";
+    $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
+    $log .= print_r($data, 1);
+    $log .= "\n------------------------\n";
+    file_put_contents(getcwd() . '/gettask.log', $log, FILE_APPEND);
+    return true;
 }
-
-?>
